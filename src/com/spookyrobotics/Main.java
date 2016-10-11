e10cad090812f2e4e10902fb2536ee439e3a4a2f
jsjrobotics <jsjrobotics@gmail.com>
2016-10-10 21:36:59 -0400

    update port

--- src/com/spookyrobotics/Main.java
+++ src/com/spookyrobotics/Main.java
@@ -5,7 +5,7 @@ import org.eclipse.jetty.server.Server;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        Server server = new Server(9000);
         server.setHandler(new VehicleServer());
 
         server.start();
